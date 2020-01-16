e03ea9abf38b66f42f391d6b05df2b42d82d1340
Oliver Almalel <oliver.almalel@gmail.com>
2020-01-16 14:28:47 -0800

    create TodoItem Components

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
+
 export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
 
   constructor() { }
 
