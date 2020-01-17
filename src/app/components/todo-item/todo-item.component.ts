15f178fe5e5541f7712b893014e39edb0cc02f23
Oliver Almalel <oliver.almalel@gmail.com>
2020-01-16 17:13:31 -0800

    complete getTodos and toggleCompleted Observable methods

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from 'src/app/models/Todo'
-
+import { TodoService } from '../../services/todo.service'
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
@@ -10,7 +10,7 @@ import { Todo } from 'src/app/models/Todo'
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit() {
   }
@@ -27,8 +27,11 @@ export class TodoItemComponent implements OnInit {
   }
 
   onToggle(todo) {
+    //Toggle on UI
     console.log('toggle')
     todo.completed = !todo.completed
+    // Toggle on Server
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo))
   }
 
   onDelete(todo) {
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -2,6 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Todo } from '../models/Todo'
 import { Observable } from 'rxjs';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type' : 'application/json'
+  })
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -11,6 +18,7 @@ export class TodoService {
   todosLimit:number = 5
   constructor(private http:HttpClient) { }
 
+  // Get Todos
   getTodos():Observable<Todo[]> {
     return this.http.get<Todo[]>(`${this.todosURL}?_limit=${this.todosLimit}`)
     // return [
@@ -30,6 +38,11 @@ export class TodoService {
     //     completed: false,
     //   },
     // ]
+  }
 
+  // Toggle Completed
+  toggleCompleted(todo: Todo):Observable<any> {
+    const url = `${this.todosURL}/${todo.id}`
+    return this.http.put(url, todo, httpOptions)
   }
 }
